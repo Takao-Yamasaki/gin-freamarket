d88a2788ed2039092c7c73db202cf62129d4ae52
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2024-01-25 22:35:21 +0900

    add:findall

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 
+	"gin-fleamarket/controllers"
 	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+	"gin-fleamarket/services"
 )
 
 func main() {
@@ -12,6 +15,11 @@ func main() {
 		{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
 		{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
 	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemService)
+
 	// ginのデフォルトルーターを指定し、rに格納
 	r := gin.Default()
 	// ルーターにエンドポイントを追加
@@ -20,6 +28,8 @@ func main() {
 			"message": "pong",
 		})
 	})
+	// 第二引数に実行する関数そのものを渡す
+	r.GET("/items", itemController.FindAll)
 	//8080でサーバーを起動
 	r.Run("localhost:8080")
 }
