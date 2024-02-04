2fe6df8751797e627234ac4ac65cb4f8948a7e5b
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2024-02-05 08:10:22 +0900

    add:setup

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 
 	"gin-fleamarket/controllers"
 	"gin-fleamarket/infra"
@@ -9,18 +10,8 @@ import (
 	"gin-fleamarket/services"
 )
 
-func main() {
-	infra.Initialize()
-	db := infra.SetupDB()
-
-	// デバッグ用
-	// log.Println(os.Getenv("ENV"))
-	// items := []models.Item{
-	// 	{Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
-	// 	{Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
-	// 	{Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
-	// }
-
+// ルーターの定義
+func setupRouter(db *gorm.DB) *gin.Engine {
 	// itemRepository := repositories.NewItemMemoryRepository(items)
 	itemRepository := repositories.NewItemRepository(db)
 	itemService := services.NewItemService(itemRepository)
@@ -30,10 +21,11 @@ func main() {
 	authServive := services.NewAuthService(authRepository)
 	authController := controllers.NewAuthController(authServive)
 
-	// ginのデフォルトルーターを指定し、rに格納
-	r := gin.Default()
 	// ルーターにエンドポイントを追加
 	// 第二引数に実行する関数そのものを渡す
+	// ginのデフォルトルーターを指定し、rに格納
+	r := gin.Default()
+
 	authRouter := r.Group("/auth")
 	authRouter.POST("/signup", authController.Signup)
 
@@ -49,6 +41,21 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	return r
+}
+
+func main() {
+	infra.Initialize()
+	db := infra.SetupDB()
+	r := setupRouter(db)
+	// デバッグ用
+	// log.Println(os.Getenv("ENV"))
+	// items := []models.Item{
+	// 	{Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
+	// 	{Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
+	// 	{Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
+	// }
 
 	//8080でサーバーを起動
 	r.Run("localhost:8080")
